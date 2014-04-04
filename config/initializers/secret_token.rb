707fd64246122a89035cf7ae1390b5c7cadfcb13
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-04 14:06:53 -0400

    rails project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpaceSpotter::Application.config.secret_key_base = '382c9e4ab9311202c4b157db7bbb5af5c8d1bc25ffea38d3807b471f09c1e81c0cf48d882503b4f737098ff1d33f2a15e3321c92caff80ee67807e35909fdbb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpaceSpotter::Application.config.session_store :cookie_store, key: '_space_spotter_session'
