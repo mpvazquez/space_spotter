89213e50aeb982c2ba512ad13cf0a2757e7174b2
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-25 16:30:14 -0400

    update to UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,21 @@
 //= require turbolinks
 //= require_tree .
 
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 $(document).ready(function() {
 
   function listingUpdate() {
@@ -24,5 +39,4 @@ $(document).ready(function() {
       li.addID('.booked');
     }
   }
-
 });
